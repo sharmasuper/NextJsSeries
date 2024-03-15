b1bfef08a80b2637cd97d8bab9a5b35224396c9f
sharmasuper <mohit6375sharma@gmail.com>
2024-03-15 21:28:15 +0530

    event use in Next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,21 @@
 
+'use client'
 import styles from "./page.module.css";
 
 export default function Home() {
+  function apple(data) {
+    alert(data)
+  }
   return (
     <main className={styles.main}>
     <h1>Home Page</h1>     
-  <User name="mohit props"/>
  
-  <Mohit name="defference"/>
+ <button onClick={()=>alert("hello Next js")}>Click Me</button>
+  {/* if not write use client in top of component it give error * we always write on the top of component  if home component have any child who import in page.js we did'nt 
+     need to write use client again and again
+  */}
+  <button   onClick={()=>apple("mango")}>apple</button>
     </main>
   );
 }
 
-  const User = (props) =>{
-  return (
-    <div>
-    <h1>User name is {props.name} </h1>
-    </div>
-  )
-}
-export const  Mohit = ({name}) =>{
-  return (
-    <div>
-     <h1>both between props -{name} </h1>
-    </div>
-  )
-}
