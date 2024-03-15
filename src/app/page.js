6c53c533015aba673337759573d90fcae8607ed4
sharmasuper <mohit6375sharma@gmail.com>
2024-03-16 00:27:16 +0530

    basic routing make folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,12 @@
 
 'use client'
-import { useState } from "react";
+
 import styles from "./page.module.css";
-
 export default function Home() {
-
-  //use satate
-  const [name,setname] = useState("hanuman")
-
-  function apple () {
-  setname("ganesh")
-  }
-  
- const InnerComponent = () =>{
-  return (
-    <>
-     <h1>Inner - Component Use</h1>
-    </>
-  )
- }
- 
   return (
     <main className={styles.main}>
     <h1>Home Page</h1>     
-    <h1>Event,function,and State - {name}</h1>
- <button onClick={()=>apple()}>ClickMe</button>
- 
- <InnerComponent/>
- {/* 2nd method */}
- {InnerComponent()}
- 
+    
     </main>
   );
 }
