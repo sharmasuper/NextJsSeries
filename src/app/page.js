97c10214b271ec571439133ba854e20e2e929917
sharmasuper <mohit6375sharma@gmail.com>
2024-03-15 21:00:34 +0530

    props passing data

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,24 @@ export default function Home() {
   return (
     <main className={styles.main}>
     <h1>Home Page</h1>     
-
+  <User name="mohit props"/>
+ 
+  <Mohit name="defference"/>
     </main>
   );
+}
+
+  const User = (props) =>{
+  return (
+    <div>
+    <h1>User name is {props.name} </h1>
+    </div>
+  )
+}
+export const  Mohit = ({name}) =>{
+  return (
+    <div>
+     <h1>both between props -{name} </h1>
+    </div>
+  )
 }
