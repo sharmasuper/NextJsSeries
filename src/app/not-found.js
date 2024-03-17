3c010df507e2f6bc73ffdc1ae7b618c3fc190242
sharmasuper <mohit6375sharma@gmail.com>
2024-03-17 20:03:02 +0530

    middleware feateure chapter in Next js

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+
+
+function notfound() {
+  return (
+    <div>
+      <h1>This page is not available</h1>
+    </div>
+  );
+}
+
+export default notfound;
---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+
+
+export function middleware(request){
+    // if(request.nextUrl.pathname!=="/Login"){
+ return NextResponse.redirect(new URL("/Login",request.url))
+    // }
+}
+
+// issai hum about ko login per redirect kartai h
+// issai ek code ko redirect kartai one more code 
+// export const config = {
+//     matcher : "/About/:path*"
+// }
+
+//one more code ko redirect karna we use Array
+export const config = {
+    matcher : ["/About/:path*","/StudentList/:path*"]
+}
