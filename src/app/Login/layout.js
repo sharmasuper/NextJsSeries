1519fac318828baf262d58376f1e624e849fb218
sharmasuper <mohit6375sharma@gmail.com>
2024-03-17 11:12:37 +0530

    lecture -11 condition layout

--- src/app/Login/layout.js
+++ src/app/Login/layout.js
@@ -1,22 +1,31 @@
+'use client'
 import Link from "next/link";
 import './Login.css'
+import { usePathname } from "next/navigation";
 
 function layout({children}) {
+
+const pathName = usePathname()
+console.log(pathName)
+
   return (
     <div>
-    <ul className="login-menu">
-        <li>
-            <h4>Login Navbar</h4>
-        </li>
-        <li>
-            <Link href="/Login">Login Main</Link>  </li>
-            <li>  <Link href="/Login/LoginStudent">Student Login</Link>  </li>
-            <li>   <Link href="/Login/LoginTeacher">Teacher Login</Link>  </li>
-            
-    </ul>
+       {pathName != "/Login/LoginTeacher"?
+         <ul className="login-menu">
+         <li>
+             <h4>Login Navbar</h4>
+         </li>
+         <li>
+             <Link href="/Login">Login Main</Link>  </li>
+             <li>  <Link href="/Login/LoginStudent">Student Login</Link>  </li>
+             <li>   <Link href="/Login/LoginTeacher">Teacher Login</Link>  </li>
+       </ul>
+       :
+       <Link href="/Login">Go To Main Login Page</Link>
+        // null
+    }
       {children}
-      {/* yai children kaya karaiga login folder kai jitnai bhi component h jaisai
-       LoginStudent or LoginTeacher un sab per yai common show karvaiga */}
+     
     </div>
   );
 }
