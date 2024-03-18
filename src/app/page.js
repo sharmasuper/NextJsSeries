902616d476b90ca521edc36a2731d10c6fb29f38
sharmasuper <mohit6375sharma@gmail.com>
2024-03-18 22:44:07 +0530

    module css use

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 
 import styles from "./page.module.css";
-import './style.css'
+import customer from'./custom.module.css'
+import other from  './other.module.css'
+import outside from '@/style/outside.module.css'
 
 export default function Home() {
   
@@ -9,8 +11,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
    
-  <h1>style and css with Next js</h1>
-  <h2>Heading 2 in main home page</h2>
+  <h1 className={customer.headers} >style and css with Next js</h1>
+  <h2 className={other.headers}>Heading 2 in main home page</h2>
+  <h3 className={outside.headers}>outside import module class use  </h3>
+  
     
     </main>
   );
