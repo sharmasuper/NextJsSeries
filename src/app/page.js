e3ed19f63f6fe8284b045ef98735a8681a24060e
sharmasuper <mohit6375sharma@gmail.com>
2024-03-19 00:41:16 +0530

    conditional style in Next js module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,27 @@
-
+'use client'
 import styles from "./page.module.css";
 import customer from'./custom.module.css'
-import other from  './other.module.css'
-import outside from '@/style/outside.module.css'
+import { useState } from "react";
 
 export default function Home() {
-  
 
-  
+  const [state,setState] = useState("red")
+  const {orange} = customer
+    
   return (
     <main className={styles.main}>
    
-  <h1 className={customer.headers} >style and css with Next js</h1>
-  <h2 className={other.headers}>Heading 2 in main home page</h2>
-  <h3 className={outside.headers}>outside import module class use  </h3>
+  <h1 className={state==="red"?customer.red:customer.green} >style and css with Next js</h1>
+  <h1 style={{backgroundColor:state==="red"?"red":"green"}}>style background property</h1>
   
+
+  <button onClick={()=>setState("green")}>Change here</button>
+ 
+  {/* id kai base per style condition */}
+
+ <h1 id={customer.orange}>heading 3 id is comming here</h1>
+
+ <h1 id={orange}>heading 3 id is comming here</h1>
     
     </main>
   );
