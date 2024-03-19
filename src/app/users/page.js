a627cb10d874ab3bf32abd699c959ef2a53fe54b
sharmasuper <mohit6375sharma@gmail.com>
2024-03-19 19:54:41 +0530

    dynmic meta data title or description

---
+++ src/app/users/page.js
@@ -0,0 +1,26 @@
+
+
+function page() {
+  return (
+    <div>
+      <h1>
+        User page
+      </h1>
+    </div>
+  );
+}
+
+export default page;
+
+
+export function generateMetadata({params}){
+    return {
+        title:"user mohit sharma",
+        description:"user page desciption"
+        
+    }
+}
+
+
+
+
