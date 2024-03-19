fa62f6d4762347fabf9f0de70b268251a7bed4b8
sharmasuper <mohit6375sharma@gmail.com>
2024-03-19 20:33:15 +0530

    script component in Nextjs

---
+++ public/Location.js
@@ -0,0 +1,20 @@
+
+console.log("location file is Lodaded")
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  console.log(position)
+}
+
+getLocation()
+
+
+
+
+
