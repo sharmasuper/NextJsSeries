65999063653b47a2313c89b419d7266ed2844fbb
sharmasuper <mohit6375sharma@gmail.com>
2024-03-20 22:35:14 +0530

    Nextjs Redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     // agar yai line nahi likhgai to hamara static html export nahi hoga
+    redirects : async()=>{
+      return [
+         {
+            source:'/Redirection',
+            destination :"/",
+            permanent : false
+         },
+         //or bhi asai add kar sktai h
+       {
+         source:'/About',
+         destination:'/user',
+         permanent:false
+       },
+       {
+         source:'/user/:userId',    //islkko batana padaiga ki dynmic h static nahi h isliy :userId likh userId ki jagah
+         destination:'/',
+         permanent:false
+       },
+       //agar hum vo url dai jo ismai access nahi kartha h to yai uskao bhi redirect kar dega
+       {
+         source:'/collage',
+         destination:"/",
+         permanent:'false'
+       }
+
+      ]
+    }
+   
 };
 
 export default nextConfig;
