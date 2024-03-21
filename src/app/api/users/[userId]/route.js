7ae04ad63318adca9cc5e48cbfbc85f439be6fdd
sharmasuper <mohit6375sharma@gmail.com>
2024-03-22 03:31:51 +0530

    Make PUT API IN NEXT JS

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,6 +1,7 @@
 
 import { user } from "@/util/db";
 
+
 import { NextResponse } from "next/server";
 
 export function GET (request,content) {
@@ -8,9 +9,25 @@ export function GET (request,content) {
     const data = user;
       const userdata = data.filter((item)=>item.age === content.params.userId)
 
-
-
      return NextResponse.json(userdata.length === 0? {result:"No data found",success : false}:{result:userdata[0],success:true },{status:200})
+}
+
+//put api bana rahahu taki ek data ko access kar usmai changes kar sku
+export async function PUT(request,content){
+  let payload  = await request.json()
+       // let age = content.params.age;
+        payload.age = content.params.userId
+        console.log("age payload",payload)
+        //validation
+      //  ismai !payload.age condition nahi lagagi bacause isper hmanai id ki condition lagai thi
+        if( !payload.age || !payload.name || !payload.email){
+          return NextResponse.json({result:"data is not valid",success:false},{status:400})
+        }
+          return NextResponse.json({result:payload,success:true},{status:200})
+        
+    
+    
+
 }
 
 
