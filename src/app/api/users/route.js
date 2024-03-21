59fb12a6c4740c0222c00633efe63fc1a5e7d151
sharmasuper <mohit6375sharma@gmail.com>
2024-03-21 23:40:32 +0530

    Post APi in Nextjs and thunderClient per check kiya bar bar

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -9,6 +9,26 @@ export function GET () {
 
 
 
+export async function POST(request){    //two paramter are have request and response
+                               //console.log(request)
+   
+   let payload = await request.json()
+
+                                                         //   console.log(payload.name)                   // console.log(payload)
+         console.log(payload)   //console.log(payload.name)     // console.log(payload)   // yai sab thunderClient per request send karkai terminal per check karna h
+   
+    if(!payload.name || !payload.age || !payload.email){
+        return NextResponse.json({result:"require field not found"})
+    }
+   return NextResponse.json({result:"hello",success : true},{status:201})
+
+    //post api ko hum asai check nahi kar stai isliy hum postman or visual studeo code mai request send kartai h thunderclient per
+
+}
+
+
+
+
 
 
 
